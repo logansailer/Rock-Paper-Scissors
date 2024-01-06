0ad7923ed040d2f920e5b4b0f0f672800836fa25
Logan <logansailer0633@gamil.com>
2024-01-07 00:04:54 +0900

    finished game, working on scoring

--- myscripts.js
+++ myscripts.js
@@ -8,37 +8,44 @@ function getPlayerChoice() {
     return (player.toUpperCase())
 }
 
-function playRound(cpuChoice, playerChoice) {
-    if (playerChoice == "ROCK" && cpuChoice == "SCISSORS") {
+function playRound() {
+    let cpuChoice = getComputerChoice()
+    let playerChoice = getPlayerChoice()
 
-    } else if (playerChoice == "ROCK" && cpuChoice == "PAPER") {
+    if (playerChoice == "ROCK" && cpuChoice == "PAPER") {
         console.log("You Lose! Paper beats Rock")
+        wl[1]++
     } else if (playerChoice == "ROCK" && cpuChoice == "SCISSORS") {
         console.log("You Win! Rock beats Scissors")
+        wl[0]++
     } else if (playerChoice == "ROCK" && cpuChoice == "ROCK") {
-        
+        playRound()
     } else if (playerChoice == "PAPER" && cpuChoice == "SCISSORS") {
         console.log("You Lose! Scissors beats Paper")
+        wl[1]++
     } else if (playerChoice == "PAPER" && cpuChoice == "ROCK") {
         console.log("You Win! Paper beats Rock")
+        wl[0]++
     } else if (playerChoice == "PAPER" && cpuChoice == "PAPER") {
-
+        playRound()
     } else if (playerChoice == "SCISSORS" && cpuChoice == "ROCK") {
         console.log("You Lose! Rock beats Scissors")
+        wl[1]++
     } else if (playerChoice == "SCISSORS" && cpuChoice == "PAPER") {
         console.log("You Win! Scissors beats Paper")
+        wl[0]++
     } else {
-
+        playRound()
     }
-
+    console.log(wl)
+    return wl
 }
 
-let cpuChoice = getComputerChoice()
-let playerChoice = getPlayerChoice()
-playRound(cpuChoice, playerChoice)
+function game() {
+for (let i = 0; i < 5; i++) {
+    playRound()
+    let wl = [0, 0]
+}
+}
 
-if 
-console.log(cpuChoice)
-console.log(playerChoice)
-
-if 
+game()
